27b386c60369655c90470cbcc5340c6fef59d81d
Anmol Sahu <anmolsahu2k@gmail.com>
2021-11-10 00:40:38 +0530

    Add theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+// import { createStore, applyMiddleware, compose } from 'redux'
+// import thunk from 'redux-thunk'
 
-import reducers from './reducers';
+// import reducers from './reducers';
 
-import App from './App'
+// import App from './App'
 
-import './index.css';
+// import './index.css';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     document.getElementById('root')
+// )
+
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+
+// ----------------------------------------------------------------------
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <HelmetProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </HelmetProvider>,
     document.getElementById('root')
-)
+);
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
